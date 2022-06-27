a536c6f7b03b3d1f87ecb4295a0de7cadd8477e4
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-06-27 04:52:41 +0300

    Edited the jwt apis function

--- src/users/controller.js
+++ src/users/controller.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
 // const salt = require('salt');
 
+
 async function getUser(req, res) {
     try {
         console.log(req.query);
@@ -26,39 +27,50 @@ async function getUser(req, res) {
 }
 
 async function register(req, res) {
+    const { email, password } = req.body
 
     try {
         // return res.status(200).send("jasjdb")
         console.log(req.body);
 
         const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(req.body.password, salt);
+        const hashPassword = await bcrypt.hash(password, salt);
 
         const addUserResult = await addUser(req.body, hashPassword);
         console.log('addUserResult =>', addUserResult);
-
-        return res.send({ user: addUserResult._id });
+        const token = jwt.sign(
+            {
+                _id: addUserResult._id,
+                //  name: user.name,
+                email
+            }, "secret"
+            // "09f26e402586e2faa8da4c98a35f1b20d6b033c60"
+        );
+        return res.status(200).json({ user: addUserResult._id, token });
     } catch (error) {
         console.log(error);
     }
 }
 
 async function login(req, res) {
+    const { email, password } = req.body
     try {
-        const user = await getByEmail(req.body.email);
+        const user = await getByEmail(email);
         if (!user) return res.status(400).send('invalid credentials');
 
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        const validPassword = await bcrypt.compare(password, user.password);
         if (!validPassword) return res.status(400).send('invalid credentials');
 
         const token = jwt.sign(
-            { _id: user._id,
+            {
+                _id: user._id,
                 //  name: user.name,
-                 email: user.email },"secret"
+                email: user.email
+            }, "secret"
             // "09f26e402586e2faa8da4c98a35f1b20d6b033c60"
         );
 
-        return res.header('auth-token', token).send(token);
+        return res.status(200).json({ user: user._id, token });
     } catch (error) {
         console.log(error);
         res.status(500).send(error);

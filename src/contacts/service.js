871b0167c9dcab0774c40bc61855cabb90c42b16
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-07-01 18:16:48 +0300

    Edit the functions of some apis for contact

--- src/contacts/service.js
+++ src/contacts/service.js
@@ -1,40 +1,23 @@
 const Contact = require("../model/Contact");
 
 
-async function getContacts(){
+async function getContacts() {
     return await Contact.find();
 }
 
-async function getContactByUserId(user){
+async function getContactByUserId(user) {
     return await Contact.find({
         user
     })
 }
 
-async function getContactById(_id){
-    return await Contact.find({
-        _id
-    })
+async function getContactById(_id) {
+    return await Contact.findById(_id)
 }
-async function addContact(body){
-    const {
-        name,
-        phone_number,
-        email,
-        relation_status,
-        loctaion,
-        user,
-
-    }=body
-    const contact = new Contact({
-        name,
-        phone_number,
-        email,
-        relation_status,
-        loctaion,
-        user
-    })
+async function addContact(body) {
     
+    const contact = new Contact(body)
+
     return await contact.save();
 }
 
